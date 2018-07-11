5b6c8e9b9f3636ff98cf852dbf7cad87a29df585
shankar625 <suryashankar625@gmail.com>
2018-07-11 13:22:55 +0530

    fisi

---
+++ if.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+a=20
+b=30 
+ if [ $a -gt $b ]; then
+   echo "$a values are greater then $b"
+ elif [ $a -eq $b ]; then
+   echo " $a values are equal $b"
+ else
+   echo "$a values are lessthen $b"
+ fi  
+ 
+  
